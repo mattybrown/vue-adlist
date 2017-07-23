917ccb23e34c778c280155b8d80856e99d522ac9
Matt Brown <brownprobable@gmail.com>
2017-07-23 17:48:46 +1200

    Finished reactive updating of ad. Styled it up pretty too.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <div class="container">
-      <article class="message is-danger" v-for="error of errors" v-if="errors">
-        <div class="message-header">
-          <p>Error: {{ error.message }}</p>
-          <button class="delete"></button>
-        </div>
-        <div class="message-body">
-          <p>Response: {{ error.response }}</p>
-          <p>Request: {{ error.request }}</p>
-          <p>{{ error }}</p>
-        </div>
-      </article>
+      <transition mode="out-in" name="error-message">
+        <article class="message is-danger" v-for="error of errors" v-if="message">
+          <div class="message-header">
+            <p>Error: {{ error.message }}</p>
+            <button class="delete" v-on:click="message = !message"></button>
+          </div>
+          <div class="message-body">
+            <p>Response: {{ error.response }}</p>
+            <p>Request: {{ error.request }}</p>
+            <p>{{ error }}</p>
+          </div>
+        </article>
+      </transition>
       <h1 class="title">{{ msg }}</h1>
 
       <table class="table is-narrow">
@@ -44,8 +46,8 @@
           </tr>
         </tfoot>
         <tbody>
-          <tr v-for="ad of ads">
-            <template v-if="ad.placed == false">
+          <template v-for="ad of ads">
+            <tr v-bind:class="{ 'table-is-approved': ad.approved }">
               <td>{{ ad.id }}</td>
               <td>{{ ad.customer }}</td>
               <td>{{ ad.rep }}</td>
@@ -55,8 +57,13 @@
               <td>?</td>
               <td>?</td>
               <td>
-                <a class="button is-small is-info" v-on:click="approveAd(ad.id)">
-                  Approve
+                <a class="button is-small is-info" v-bind:class="{ 'is-inverted': ad.approved }" v-on:click="approveAd(ad.id)">
+                  <template v-if="ad.approved">
+                    Approved
+                  </template>
+                  <template v-else>
+                    Approve
+                  </template>
                 </a>
               </td>
               <td>
@@ -64,8 +71,8 @@
                   Place
                 </a>
               </td>
-            </template>
-          </tr>
+            </tr>
+          </template>
         </tbody>
       </table>
     </div>
@@ -80,7 +87,8 @@ export default {
     return {
       ads: [],
       errors: [],
-      msg: "Ad list"
+      msg: "Ad list",
+      message: true
     }
   },
   methods: {
@@ -90,18 +98,21 @@ export default {
           this.ads = response.data
         })
         .catch(e => {
-          this.errors.push(e)
+          this.errors.push(e);
+          this.message = true
         })
     },
     approveAd: function (id) {
-      HTTP.post('ad/approve/', {
+      HTTP.post('ad/approve', {
         ad_id: id
       }).then(response => {
-        console.log(response)
+        console.log(response);
+        this.loadData();
       }).catch(e => {
-        this.errors.push(e)
+        this.errors.push(e);
+        this.message = true
       })
-    },
+    }
   },
   mounted: function () {
     this.loadData();
@@ -110,6 +121,30 @@ export default {
 </script>
 
 <style lang="sass">
+
+$white-ter:    hsl(0, 0%, 96%) !default
+$white-bis:    hsl(0, 0%, 98%) !default
+$white:        hsl(0, 0%, 100%) !default
+
+$green:        hsl(141, 71%,  48%) !default
+$turquoise:    hsl(171, 100%, 41%) !default
+$blue:         hsl(217, 71%,  53%) !default
+
 .table
   font-size: .9em
+
+.error-message-enter-active, .error-message-leave-active
+  transition: all .5s ease
+
+.error-message-enter, .error-message-leave-to
+  opacity: 0
+
+tr
+  &.table-is-approved
+    background-color: $blue
+    color: $white-ter
+    &:hover
+      background-color: $blue
+
+
 </style>
